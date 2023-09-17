5d7270ab344f9224becdb86084ffc7e012302c92
Enrico Kälin <97579163+Tschiggo87@users.noreply.github.com>
2023-09-17 08:29:32 +0200

    Add PopUp Buttons

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const closeButton = document.getElementById('closeButton');
+    const goToContact = document.getElementById('goToContact');
     const popUpWrapper = document.getElementById('popUpWrapper');
 
     closeButton.addEventListener('click', () => {
         popUpWrapper.style.display = 'none';
     });
+
+    goToContact.addEventListener('click', () => {
+        popUpWrapper.style.display = 'none';
+        window.location.hash = 'contact';
+    });
+
 });
+
 
